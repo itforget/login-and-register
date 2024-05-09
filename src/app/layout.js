c2997d5b7953cb7b18c09f2728d9a07d3c0c3d2d
itforget CORDEIRO <itforget11@gmail.com>
2024-05-09 16:55:50 +0000

    change font family

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { AR_One_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ar_one_sans = AR_One_Sans({ 
+  subsets: ["latin"], 
+  weight: [ "400"]
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ar_one_sans.className}>{children}</body>
     </html>
   );
 }
